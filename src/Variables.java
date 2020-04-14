97664d4f50e9ed37f5970daa380d66f20b069c6b
azdoolittle <drewdoolittle@gmail.com>
2020-04-14 10:59:42 -0400

    declared 3 vars and printed them out

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables
+{
+    public static void main(String[] argv)
+    {
+        byte testScore = 127;
+        float price = 9.99f;
+        String username = "myUsername";
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + username);
+    }
+
+}
